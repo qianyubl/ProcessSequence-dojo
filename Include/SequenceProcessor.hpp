2f5bd89214a8b6c035179fc0e6604f7a2ed8c5e4
qianyubl <longjourney123@gmail.com>
2017-02-23 18:41:15 +0800

    refactor the process function and spilt it into several functions

--- Include/SequenceProcessor.hpp
+++ Include/SequenceProcessor.hpp
@@ -5,5 +5,12 @@ class SequenceProcessor
 {
 public:
     static void process(vector<int>& p_seq);
+    
+    static void handleInvalidSeq(vector<int>& p_seq);
+    static void sortSeq(vector<int>& p_seq);
+    static void removeDuplicateNums(vector<int>& p_seq);
+    static void putNumsCannotBeDevidedBy5Back(vector<int>& p_seq, vector<int>::iterator &p_result);
+    static void twoLastNumbersDivededBy5ShouldbeExchangedWithNext2Numbers(vector<int>& p_seq, vector<int>::iterator &p_result);
+    static void putBackAllNumbersBeforeThe1stNumberBiggerThanThe1OnTheFront(vector<int>& p_seq, vector<int>::iterator &p_result); 
 };
 
--- Source/SequenceProcessor.cpp
+++ Source/SequenceProcessor.cpp
@@ -4,6 +4,19 @@
 
 void SequenceProcessor::process(vector<int>& p_seq)
 {
+    
+    handleInvalidSeq(p_seq);
+    sortSeq(p_seq);
+    removeDuplicateNums(p_seq);
+    vector<int>::iterator l_result;
+    putNumsCannotBeDevidedBy5Back(p_seq,l_result);
+    twoLastNumbersDivededBy5ShouldbeExchangedWithNext2Numbers(p_seq,l_result);
+    putBackAllNumbersBeforeThe1stNumberBiggerThanThe1OnTheFront(p_seq,l_result);
+    
+}
+
+void SequenceProcessor::handleInvalidSeq(vector<int>& p_seq)
+{
     if(p_seq.size() != 8)
     {
         throw length_error("invalid size");
@@ -15,18 +28,37 @@ void SequenceProcessor::process(vector<int>& p_seq)
         throw out_of_range("Numbers out of range.");
     }
 
-    sort(p_seq.begin(), p_seq.end());
-
-    p_seq.erase(unique(p_seq.begin(), p_seq.end()), p_seq.end());
-
-    auto l_result = stable_partition(p_seq.begin(), p_seq.end(),
-                                     [](auto p_val){return p_val % 5 == 0;});
-
-    if(2 <= distance(p_seq.begin(), l_result) and 2 <= distance(l_result, p_seq.end()))
-    {
-        reverse(prev(l_result, 2), next(l_result, 2));
-    }
-
-    l_result = find_if(p_seq.begin(), p_seq.end(), [&](int p_val){return p_val > p_seq[0];});
-    rotate(p_seq.begin(), l_result, p_seq.end());
 }
+
+void SequenceProcessor::sortSeq(vector<int>& p_seq)
+{
+    sort(p_seq.begin(), p_seq.end());
+}
+
+void SequenceProcessor::removeDuplicateNums(vector<int>& p_seq)
+{
+    p_seq.erase(unique(p_seq.begin(), p_seq.end()), p_seq.end());    
+}
+
+void SequenceProcessor::putNumsCannotBeDevidedBy5Back(vector<int>& p_seq, vector<int>::iterator &p_result)
+{
+    p_result = stable_partition(p_seq.begin(), p_seq.end(),
+                                [](auto p_val){return p_val % 5 == 0;});
+}
+
+void SequenceProcessor::twoLastNumbersDivededBy5ShouldbeExchangedWithNext2Numbers(vector<int>& p_seq, vector<int>::iterator &p_result)
+{
+    if(2 <= distance(p_seq.begin(), p_result) and 2 <= distance(p_result, p_seq.end()))
+    {
+        reverse(prev(p_result, 2), next(p_result, 2));
+    }
+}
+
+void SequenceProcessor::putBackAllNumbersBeforeThe1stNumberBiggerThanThe1OnTheFront(vector<int>& p_seq, vector<int>::iterator &p_result)
+{
+    p_result = find_if(p_seq.begin(), p_seq.end(), [&](int p_val){return p_val > p_seq[0];});
+    rotate(p_seq.begin(), p_result, p_seq.end());
+
+}
+
+
