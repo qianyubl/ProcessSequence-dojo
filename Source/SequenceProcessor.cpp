7a476102644a006b2ed805c4d9416ff401aaba0f
zcphoenix2016 <13564240769@139.com>
2017-02-23 16:12:52 +0800

    refactoring

--- Source/SequenceProcessor.cpp
+++ Source/SequenceProcessor.cpp
@@ -22,11 +22,9 @@ void SequenceProcessor::process(vector<int>& p_seq)
     auto l_result = stable_partition(p_seq.begin(), p_seq.end(),
                                      [](auto p_val){return p_val % 5 == 0;});
 
-    auto l_first = prev(l_result, 2);
-    auto l_last = next(l_result, 2);
-    if(l_first >= p_seq.begin() and l_last <= p_seq.end())
+    if(2 <= distance(p_seq.begin(), l_result) and 2 <= distance(l_result, p_seq.end()))
     {
-        reverse(l_first, l_last);
+        reverse(prev(l_result, 2), next(l_result, 2));
     }
 
     l_result = find_if(p_seq.begin(), p_seq.end(), [&](int p_val){return p_val > p_seq[0];});
