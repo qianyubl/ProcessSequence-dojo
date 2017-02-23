8b09126a33c742cb9f28e03a21756b1afe4b20b4
zcphoenix2016 <13564240769@139.com>
2017-02-23 10:56:14 +0800

    the implemtation of put the numbers can not be devided by 5 back

--- Source/SequenceProcessor.cpp
+++ Source/SequenceProcessor.cpp
@@ -7,5 +7,8 @@ int SequenceProcessor::ProcessSequence(vector<int>& p_seq)
     auto l_last = unique(p_seq.begin(), p_seq.end());
     p_seq.erase(l_last, p_seq.end());
 
+    stable_partition(p_seq.begin(), p_seq.end(),
+                     [](auto p_val){return p_val % 5 == 0;});
+
     return 0;
 }
--- Test_modules/SequenceProcessorTestSuite.cpp
+++ Test_modules/SequenceProcessorTestSuite.cpp
@@ -5,16 +5,24 @@ using namespace std;
 
 TEST(SequenceProcessorTestSuite, anUnsortedSequenceShouldBeSorted)
 {
-    vector<int> l_seq = { 5, 8, 0, 100, 95, 76, 25, 42 };
-    vector<int> l_seqExpect = { 0, 5, 8, 25, 42, 76, 95, 100 };
+    vector<int> l_seq = { 1, 8, 7, 99, 94, 76, 22, 42 };
+    vector<int> l_seqExpect = { 1, 7, 8, 22, 42, 76, 94, 99 };
     SequenceProcessor::ProcessSequence(l_seq);
     ASSERT_EQ(l_seq, l_seqExpect);
 }
 
 TEST(SequenceProcessorTestSuite, duplicatedNumbersShouldBeRemoved)
 {
+    vector<int> l_seq = { 1, 8, 1, 99, 94, 8, 42, 42 };
+    vector<int> l_seqExpect = { 1, 8, 42, 94, 99 };
+    SequenceProcessor::ProcessSequence(l_seq);
+    ASSERT_EQ(l_seq, l_seqExpect);
+}
+
+TEST(SequenceProcessorTestSuite, allNumbersCannotBeDevidedBy5ShouldBePlacedBack)
+{
     vector<int> l_seq = { 5, 8, 0, 100, 95, 8, 42, 42 };
-    vector<int> l_seqExpect = { 0, 5, 8, 42, 95, 100 };
+    vector<int> l_seqExpect = { 0, 5, 95, 100, 8, 42 };
     SequenceProcessor::ProcessSequence(l_seq);
     ASSERT_EQ(l_seq, l_seqExpect);
 }
